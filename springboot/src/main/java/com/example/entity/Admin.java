23176a78531dcf3808f5f659046b709746c0a153
Ming <1034796585 qq.com>
2025-04-22 11:47:17 +0800

    首次提交：初始化学生选课管理系统项目

---
+++ springboot/src/main/java/com/example/entity/Admin.java
@@ -0,0 +1,61 @@
+package com.example.entity;
+
+public class Admin extends Account {
+    private Integer id;
+    // 账号
+    private String username;
+    // 密码
+    private String password;
+    //姓名
+    private String name;
+    //角色等级
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
